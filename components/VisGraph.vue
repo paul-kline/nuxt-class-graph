aa09e5ca041ae4d297418dc39476beb4dd6500ca
paul-kline <pauliankline@gmail.com>
2021-03-27 23:57:52 -0500

    yeah works about right

--- components/VisGraph.vue
+++ components/VisGraph.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    VisGraph works
     <v-autocomplete
       item-value="id"
       :item-text="e => e.id + ' ' + e.name"
@@ -8,25 +7,79 @@
       :value="selectedCourseID"
       @change="selectionChanged"
     ></v-autocomplete>
-    <div id="cy"></div>
+    <div class="d-flex">
+      <div id="cy"></div>
+      <div id="buttons" class="d-flex flex-column">
+        <v-btn @click="stabalize">stabalize</v-btn>
+        <v-btn @click="defaultLayout">Fluid Layout</v-btn>
+        <v-btn @click="heirarical">heirarical view</v-btn>
+        <v-checkbox
+          v-model="onlyImmediate"
+          :label="`Direct Requirements only.`"
+          @change="immediateChanged"
+        ></v-checkbox>
+        <!-- <v-btn @click="heirarical2">heirarical 2</v-btn> -->
+        <div v-if="selectedCourse" style="max-width:12vw">
+          <div>PreReq: {{ selectedCourse.prereqFormula || "None" }}</div>
+          <br />
+          <div>CoReq: {{ selectedCourse.coreqFormula || "None" }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 import * as vis from "vis-network";
 import * as visdat from "vis-data";
-import { CourseMaster, CourseMaster } from "~/ts/Utils";
+import { CourseMaster } from "~/ts/Utils";
 type Op = "AND" | "OR";
 type Ls = "Range" | "List";
 type List = { credits: number; ls: string[]; gpa?: number; type: Ls };
 type Conj = { op: Op; left: Req; right: Req };
 type Req = string | Conj | Course | List;
 type Course = { course: string; gpa: number };
+
 function isCourse(x: Course | Conj | List): x is Course {
   return (x as Course).course !== undefined;
 }
 function isList(x: Course | Conj | List): x is List {
   return (x as List).credits !== undefined;
+}
+function mkCourseTitle(c: CourseMaster): string {
+  return (
+    c.id +
+    " (" +
+    c.credits +
+    "cr) " +
+    "<br/>" +
+    c.name +
+    "<br/>" +
+    multiline(c.description)
+  );
+}
+function multiline(txt: string, lineWidth: number = 50) {
+  const arr = txt.split(" ");
+  let linelen = 0;
+  let answer = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (linelen < lineWidth) {
+    } else {
+      answer += "<br/>";
+      linelen = 0;
+    }
+    answer += arr[i] + " ";
+    linelen += arr[i].length + 1;
+  }
+  return answer;
+}
+
+function htmlTitle(html: string) {
+  const container = document.createElement("div");
+  // container.style.maxWidth = "50vw";
+  // container.style.overflowWrap = "normal";
+  container.innerHTML = html;
+  return container;
 }
 let currentCourse: CourseMaster | null = null; //I know this is naughty but I really really need this to prevent ranges from repeating itself as dependency.
 let network: vis.Network | null = null;
@@ -48,12 +101,53 @@ export default class VisGraph extends Vue {
 
   edges: visdat.DataSet<any, "id"> | null = null;
   selectedCourseID: string = "";
-  selectedCourse: CourseMaster | undefined = undefined;
+  selectedCourse: CourseMaster | null = null;
+  onlyImmediate: boolean = false; //only show direct pre/coreqs.
+  immediateChanged() {
+    this.selectionChanged(this.selectedCourse?.id || "");
+  }
+
+  stabalize() {
+    network?.stabilize();
+  }
+  heirarical() {
+    network?.setOptions({
+      layout: {
+        // hierarchical: {
+        //   direction: "UD",
+        //   sortMethod: "directed"
+        // }
+        hierarchical: true
+      },
+
+      physics: {
+        hierarchicalRepulsion: {
+          avoidOverlap: 1
+        }
+      }
+    });
+  }
+  heirarical2() {
+    network?.setOptions({
+      layout: {
+        hierarchical: {
+          sortMethod: "hubsize",
+          shakeTowards: "leaves" //roots
+        }
+      }
+    });
+  }
+  defaultLayout() {
+    network?.setOptions({
+      layout: { hierarchical: false, randomSeed: Math.random() * 100 }
+    });
+    network?.redraw();
+  }
   findCourse(id: string, courses = this.courses): CourseMaster | undefined {
     return courses.find(c => c.id == id);
   }
   selectionChanged(e: string) {
-    this.selectedCourse = this.findCourse(e);
+    this.selectedCourse = this.findCourse(e) || null;
     const c = this.selectedCourse;
     if (!c) {
       console.error("impossible! class e did not exist:", e);
@@ -67,7 +161,14 @@ export default class VisGraph extends Vue {
       const [nodes, edges] = this.mkNodesAndEdgesFor(c);
       //@ts-ignore
       c.color = "white";
+      //@ts-ignore
+      c.title = htmlTitle(mkCourseTitle(c));
+      //@ts-ignore
+      c.level = 1;
+      console.log("adding title", c.name);
+      // c.title = c.name;
       this.nodes.add(c);
+
       const ns = this.nodes;
       nodes.forEach(n => {
         ns.add(n);
@@ -79,11 +180,9 @@ export default class VisGraph extends Vue {
         eds.add(e);
       });
 
-      // console.log(
-      //   "finished adding edges and nodes",
-      //   this.nodes.getIds(),
-      //   this.edges.getDataSet().map(x => x)
-      // );
+      setTimeout(() => {
+        network?.fit();
+      }, 10);
       (window as any).graph = network;
       (window as any).edges = this.edges;
       if (this.nodes && this.edges) {
@@ -95,28 +194,32 @@ export default class VisGraph extends Vue {
   mkNodesAndEdgesFor(
     c: CourseMaster,
     nodes: any[] = [],
-    edges: any[] = []
+    edges: any[] = [],
+    level: number = 1
   ): [any[], any[]] {
     currentCourse = c; //I know this is naughty, but I really really need it for not duplicating list occurances in ranges.
     if (c.prereqTree) {
-      console.log(
-        "prereq exists for class:",
-        c.id,
-        "Formula: ",
-        c.prereqFormula,
-        "obj",
-        c.prereqTree
-      );
+      // console.log(
+      //   "prereq exists for class:",
+      //   c.id,
+      //   "Formula: ",
+      //   c.prereqFormula,
+      //   "obj",
+      //   c.prereqTree
+      // );
       //initialize prereqs.
       const prereqNode = {
         id: "prereq" + c.id,
         label: "PREREQS",
-        shape: SHAPES.prereq
+        shape: SHAPES.prereq,
+        title: "PreReqs for " + c.id,
+        level: level + 1
       };
       if (!nodes.find(x => x.id == "prereq" + c.id)) {
         const preReqEdge = {
           from: c.id,
-          to: "prereq" + c.id
+          to: "prereq" + c.id,
+          arrows: { from: true }
         };
         nodes.push(prereqNode);
         edges.push(preReqEdge);
@@ -127,21 +230,25 @@ export default class VisGraph extends Vue {
         true,
         true,
         nodes,
-        edges
+        edges,
+        level + 1
       );
     }
     if (c.coreqTree) {
-      console.log("COreq exists for class:", c.id, "formula: ", c.coreqFormula);
+      // console.log("COreq exists for class:", c.id, "formula: ", c.coreqFormula);
       //initialize coreqs.
       const coreqNode = {
         id: "coreq" + c.id,
         label: "COREQS",
-        shape: SHAPES.coreq
+        shape: SHAPES.coreq,
+        title: "CoReqs for " + c.id,
+        level: level + 1
       };
       if (!nodes.find(x => x.id == "coreq" + c.id)) {
         const coReqEdge = {
           from: c.id,
-          to: "coreq" + c.id
+          to: "coreq" + c.id,
+          arrows: { from: true }
         };
         nodes.push(coreqNode);
         edges.push(coReqEdge);
@@ -153,7 +260,8 @@ export default class VisGraph extends Vue {
         false,
         true,
         nodes,
-        edges
+        edges,
+        level + 1
       );
     }
     return [nodes, edges];
@@ -164,7 +272,8 @@ export default class VisGraph extends Vue {
     isPreReq: boolean,
     isAnd: boolean = true,
     nodes: any[] = [],
-    edges: any[] = []
+    edges: any[] = [],
+    level: number
   ): [any[], any[]] {
     // console.log("nodes, edges", nodes, edges);
     // console.log("processing req:", req);
@@ -179,19 +288,26 @@ export default class VisGraph extends Vue {
         to: to,
         // label: isPreReq ? "PRE" : "CO",
         color: isAnd ? "red" : "green",
+        arrows: { from: true },
         width: 2
       });
     }
-    function addNode(id: string, label: string, shape: string) {
+    function addNode(id: string, label: string, shape: string, title?: string) {
       if (nodes.find(x => x.id == id)) {
         // console.log("REFUSING TO ADD NODE (EXISTS) " + id);
       } else {
-        nodes.push({
+        const x: any = {
           id: id,
           label: label,
           shape: shape,
-          group: isPreReq ? "prereq" : "coreq"
-        });
+          level: level + 1
+          // group: isPreReq ? "prereq" : "coreq"
+        };
+        if (title) {
+          x.title = htmlTitle(title);
+          x.widthConstraint = 50;
+        }
+        nodes.push(x);
       }
     }
     if (typeof req == "string") {
@@ -215,15 +331,24 @@ export default class VisGraph extends Vue {
       } else {
         //it is a class req with no gpa.
         if (!nodes.find(x => x.id == req)) {
-          addNode(req, req, SHAPES.course);
+          const c = this.findCourse(req);
+          let title = "";
+          if (c) {
+            title = mkCourseTitle(c);
+          }
+          addNode(req, req, SHAPES.course, title);
         }
         addEdge(src.id, req);
 
         const c = this.findCourse(req);
         if (c) {
-          return this.mkNodesAndEdgesFor(c, nodes, edges);
+          if (this.onlyImmediate) {
+            return [nodes, edges];
+          } else {
+            return this.mkNodesAndEdgesFor(c, nodes, edges, level + 1);
+          }
         } else {
-          console.error("I expected to find class but I didn't. ID was: ", c);
+          console.error("I expected to find class but I didn't. ID was: ", req);
           return [nodes, edges];
         }
       }
@@ -231,13 +356,27 @@ export default class VisGraph extends Vue {
       // console.log("REQ IS: ", req);
       //if course prop exists, this is {course:str,gpa:num};
       if (!nodes.find(x => x.id == req.course)) {
-        addNode(req.course, req.course + "\ngpa: " + req.gpa, SHAPES.course);
+        let title = "";
+        const x = this.findCourse(req.course);
+        if (x) {
+          title = mkCourseTitle(x);
+        }
+        addNode(
+          req.course,
+          req.course + "\ngpa: " + req.gpa,
+          SHAPES.course,
+          title
+        );
       }
       addEdge(src.id, req.course);
 
       const c = this.findCourse(req.course);
       if (c) {
-        return this.mkNodesAndEdgesFor(c, nodes, edges);
+        if (this.onlyImmediate) {
+          return [nodes, edges];
+        } else {
+          return this.mkNodesAndEdgesFor(c, nodes, edges, level + 1);
+        }
       } else {
         console.error(
           "I expected to find class in isCourse(req) but I didn't. ID was: ",
@@ -257,6 +396,7 @@ export default class VisGraph extends Vue {
           (req.gpa ? " (GPA: " + req.gpa + ")" : "")
       };
       addNode(src2.id, src2.label, SHAPES.major);
+      level++;
       addEdge(src.id, src2.id);
       src = src2;
       let ls;
@@ -280,13 +420,20 @@ export default class VisGraph extends Vue {
           )
           .map(x => x.department + x.number);
       }
-      console.log("origID", origID, "list contains", ls);
+      // console.log("origID", origID, "list contains", ls);
       ls.forEach(c => {
         const cc = this.findCourse(c);
-        addNode(c, c, SHAPES.course);
+        addNode(c, c, SHAPES.course, cc ? mkCourseTitle(cc) : "");
         addEdge(src.id, c);
         if (cc) {
-          [nodes, edges] = this.mkNodesAndEdgesFor(cc, nodes, edges);
+          if (!this.onlyImmediate) {
+            [nodes, edges] = this.mkNodesAndEdgesFor(
+              cc,
+              nodes,
+              edges,
+              level + 1
+            );
+          }
         } else {
           console.error(
             "this shouldn't be possible. couldn't find course: ",
@@ -304,6 +451,7 @@ export default class VisGraph extends Vue {
         } else {
           const src2 = { id: src.id + "AND", label: "ALL" };
           addNode(src2.id, src2.label, SHAPES.conjuction);
+          level++;
           addEdge(src.id, src2.id);
           src = src2;
         }
@@ -314,6 +462,7 @@ export default class VisGraph extends Vue {
         } else {
           const src2 = { id: src.id + "OR", label: "ONE OF" };
           addNode(src2.id, src2.label, SHAPES.disjuction);
+          level++;
           addEdge(src.id, src2.id);
           src = src2;
         }
@@ -326,7 +475,8 @@ export default class VisGraph extends Vue {
         isPreReq,
         isAnd2,
         nodes,
-        edges
+        edges,
+        level
       );
       [nodes, edges] = this.processReqTree(
         req.right,
@@ -334,7 +484,8 @@ export default class VisGraph extends Vue {
         isPreReq,
         isAnd2,
         nodes,
-        edges
+        edges,
+        level
       );
       return [nodes, edges];
     }
@@ -362,18 +513,42 @@ export default class VisGraph extends Vue {
     );
     this.courses.forEach(c => {
       (c as any).label = c.department + " " + c.number;
-      // if (c.prereqTree) {
-      //   console.log(c.prereqTree);
-      // }
-      // if (c.coreqTree) {
-      //   console.log(c.coreqTree);
-      // }
     });
     this.mkNetwork();
+    // const sortedByComplexity = this.computeMostComplex();
+    // console.log("by complexity", sortedByComplexity);
+    // this.testAll();
+    (window as any).computeMostComplex = this.computeMostComplex;
   }
-
+  computeMostComplex() {
+    const ls = [];
+    for (let i = 0; i < this.courses.length; i++) {
+      const c = this.courses[i];
+      this.selectionChanged(c.id);
+      const nodes = this.nodes?.getIds();
+      const edges = this.nodes?.map(x => {
+        return { from: x.from, to: x.to };
+      });
+      ls.push({
+        id: c.id,
+        nodes: nodes ? nodes : [],
+        edges: edges ? edges : []
+      });
+    }
+    ls.sort(
+      (x, y) =>
+        x.nodes.length + x.edges.length - (y.nodes.length + y.edges.length)
+    );
+    return ls;
+  }
+  currentI = 0;
+  testAll() {
+    this.selectionChanged(this.courses[this.currentI].id);
+    this.currentI++;
+    setTimeout(this.testAll, 1000);
+  }
   mkNodes(): any[] {
-    return this.courses.filter(x => x.department == "CS");
+    return []; //this.courses.filter(x => x.department == "CS");
   }
   mkNetwork() {
     // var nodes = new visdat.DataSet([
@@ -403,7 +578,30 @@ export default class VisGraph extends Vue {
       nodes: this.nodes,
       edges: this.edges
     };
-    var options = {};
+    var options = {
+      nodes: {
+        level: 1
+      },
+      layout: {
+        // hierarchical: {
+        //   direction: "UD",
+        //   sortMethod: "directed"
+        // }
+        hierarchical: true
+      },
+
+      physics: {
+        hierarchicalRepulsion: {
+          avoidOverlap: 1
+        },
+        barnesHut: {
+          avoidOverlap: 1
+        },
+        forceAtlas2Based: {
+          avoidOverlap: 1
+        }
+      }
+    };
 
     // initialize your network!
     //@ts-ignore
@@ -411,7 +609,10 @@ export default class VisGraph extends Vue {
   }
 }
 </script>
-<style>
+
+<style lang="scss">
+@import url("vis-network/dist/dist/vis-network.css");
+
 #cy {
   width: 80vw;
   height: 70vh;
---
+++ pages/about.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <div>
+      <h1>Background</h1>
+      I built this tool after needing to find the entire prereq tree for a
+      class. In typical computer scientist fashion, we like to "spend 10 hours
+      automating a 5 minute task."
+      <p>
+        The first step was to harvest all course information listed in the
+        portal CourseMaster page. This is now part of the
+        <a
+          href="https://chrome.google.com/webstore/detail/cams-helper/nohnkhiaaclbbanceebnjbmchpmjgffn?hl=en-US"
+          target="_blank"
+          >Cams Helper extension</a
+        >
+        so that future changes can easily be reflected here. At the time of this
+        writing, there are 518 Blackburn courses. After all course information
+        was harvested, the co and prerequisite formulas were tokenized. Tokens
+        include brackets and parentheses, a course, a course with gpa
+        requirement, a major requirement, a class standing requirement, "AND",
+        "OR", number of credits from a list of classes, and a number of credits
+        from a range of classes with or without a GPA requirement (Ranges get
+        interpolated at time of render). Tokens are then parsed using a
+        variation of the
+        <a
+          href="https://en.wikipedia.org/wiki/Shunting-yard_algorithm"
+          target="_blank"
+          >Shunting-yard algorithm</a
+        >
+        using the AND/OR operators and the location of parentheses. The co/pre
+        req parse trees are attached to the course master data. The render
+        method handles flattening AND/OR parse trees to avoid superfluous
+        dis/con junctions.
+      </p>
+    </div>
+
+    <div>
+      Errors in requirements are the fault of incorrect requirements (usually
+      lack of parentheses) listed in the portal from where they were harvested.
+      Errors in layout are due to the visjs graph layout algorithm, which I
+      agree are frustrating, but I'm not about to write my own network graph
+      library. Sometimes, you may prefer to move things in the 'fluid' layout to
+      avoid overlaps, then change to the heirarical layout.
+    </div>
+    <div>
+      <h1>Usage</h1>
+      <h3>Finding a course</h3>
+      <p>
+        In the text box, start typing the department code followed by (no space)
+        the course number to select a course for which you wish to view the
+        pre/co reqs. You may also filter by the title of the course if the
+        dept/course number is not known.
+      </p>
+      <h3>Limiting size</h3>
+      <p>
+        By default, the dependency graph displayed is recursive-- that is to
+        say, all the pre/co reqs for all pre/co reqs (and so on and so forth)
+        are also displayed. Sometimes, this can become unwieldy (as it is with
+        ED422 for example, which has a large list to choose from). Therefore,
+        you have the option on the right hand side to select "Direct
+        Requirements only" which will only list the immediate class requirements
+        in the co/pre reqs.
+      </p>
+      <h3>Fit-to-screen</h3>
+      <p>
+        At any time, the 'stabilize' button will stop any animation and ensure
+        the entire graph is optimally displayed within the window.
+      </p>
+      <h3>Re-arranging</h3>
+      <p>
+        An alternative graph view can be employed by clicking the 'fluid layout'
+        button. This physics simulation tries to space everything equally
+        repulsively. Sometimes, you may prefer to move things in this layout to
+        avoid overlaps, then change to the heirarical layout.
+      </p>
+      <div>
+        <h1>Layout</h1>
+        <h3>Arranging</h3>
+        <p>
+          Scrolling up or down will zoom in/out. You may also click and drag the
+          background to move about, as well as click and drag nodes (if in fluid
+          layout) to move them. (see re-arranging section above).
+        </p>
+        <h3>Interpretting shapes</h3>
+        <p>
+          The selected course will appear white in the graph. All courses are
+          circles. Co and pre requisite edges are labeled. If there are multiple
+          requirements for a requisite, an upward pointing triangle (similar to
+          conjuction '/\') connects them. If there is a choice requisite
+          (commonly 'x' credits from list of classes, or sophomore standing or
+          above, for example), a downward pointing triangle (similar to
+          disjuction '\/') joins them.
+        </p>
+        <p>Class standing or Major requirements are drawn in boxes.</p>
+      </div>
+    </div>
+  </div></template
+>
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class About extends Vue {}
+</script>
+<style></style>
